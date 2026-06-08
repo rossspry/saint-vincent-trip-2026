7d8d9a30db39bef196f63aaf4cb4b06177db444a
rossspry <rossspry@gmail.com>
2026-06-08 13:32:16 -0400

    Fix family main photo cache version

--- family-main-photo.js
+++ family-main-photo.js
@@ -1,5 +1,5 @@
 window.FAMILY_MAIN_PHOTO = {
   title: "Ross Whitney Ava Ken Kelli Rylei and Grayson sailing adventure",
-  url: "family.jpg?v=20260606-frigate-2",
-  viewUrl: "family.jpg?v=20260606-frigate-2"
+  url: "family.jpg?v=20260608-main-photo-fixed",
+  viewUrl: "family.jpg?v=20260608-main-photo-fixed"
 };
